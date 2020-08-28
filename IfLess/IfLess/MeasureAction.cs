fe29094896dfa6ad7c841e89d88614369626917c
Le, Anh Tan <tan.le@zoi.de>
2020-08-28 14:24:06 +0700

    Implement IfLess technical

---
+++ IfLess/IfLess/MeasureAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IfLess
+{
+    public class MeasureAction
+    {
+        public Func<string, bool> CanAppply { get; set; }
+        public Action Action { get; set; }
+
+        public MeasureAction(Func<string, bool> canAppply, Action action)
+        {
+            CanAppply = canAppply;
+            Action = action;
+        }
+    }
+}
---
+++ IfLess/IfLess/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IfLess
+{
+    class Program
+    {
+        static void Edit()
+        {
+            Console.WriteLine("Edit");
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Add");
+        }
+
+        static void Delete()
+        {
+            Console.WriteLine("Delete");
+        }
+
+        static void Main(string[] args)
+        {
+            var measureActions = new MeasureAction[]
+            {
+                new MeasureAction(m => m == "Add", () => Add()),
+                new MeasureAction(m => m == "Edit", () => Edit()),
+                new MeasureAction(m => m == "Delete", () => Delete()),
+                new MeasureAction(m => true, () => Delete())
+            };
+            var action = "Add";
+
+            measureActions.First(x => x.CanAppply(action)).Action();
+            action = "Edit";
+            measureActions.First(x => x.CanAppply(action)).Action();
+            action = "default";
+            measureActions.First(x => x.CanAppply(action)).Action();
+            Console.Read();
+        }
+    }
+}
